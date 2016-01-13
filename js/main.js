87175925d3d500a13e819aecb7ce2b25ae63a481
horo <notender_boy@ukr.net>
2016-01-13 14:44:19 +0200

    remove old js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 $( document ).ready(function() {
 
-    var overflow_wr = $('#fixed-menu');
-
-    $('.nav-menu-btn').on('click', function(){
-        overflow_wr.fadeIn();
-    });
-    $('#nav-menu-close-btn').on('click', function(){
-        overflow_wr.fadeOut();
-    });
 
 
 
