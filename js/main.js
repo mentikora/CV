60f62d1056cef700ce833abf558aaafa65505986
horo <notender_boy@ukr.net>
2016-02-01 17:05:02 +0200

    something

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-$( document ).ready(function() {
+// var tag = document.createElement('script');
+// tag.src = "http://www.youtube.com/player_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+// var player;
+// function onYouTubePlayerAPIReady() {
+// player = new YT.Player('player', {
+//     playerVars: { 'autoplay': 1, 'controls': 1,'autohide':1,'wmode':'opaque' },
+//     videoId: 'ck0MCCIFD-o',
+//     events: {
+//        'onReady': onPlayerReady}
+//    });
+// }
+
+// function onPlayerReady(event) {
+//    event.target.mute();
+// }
+
+$( document ).ready(function() {
 
 
 
