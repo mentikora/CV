6401274ea22e73faeadb32dccd3d7215ca15dcdc
horo <notender_boy@ukr.net>
2016-02-15 13:43:38 +0200

    fx

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $('document').ready(function() {
-	var options = { videoId: 'ck0MCCIFD-o', start: 3, mute: true };
-	if ( $(document).width() > 991 ) {
-		$('#preload').tubular(options);
-	} else{
-		$('#preload').css('display', 'none');
-		$('#wrapper').css('display', 'block');
-	};
 
-	
 });
